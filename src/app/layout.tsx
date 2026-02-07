f6fb3710694714c55e289b4eb05e3762d1371a8f
Sergio Coria <sergiocoria.my@gmail.com>
2026-02-06 23:11:38 -0700

    Add Google auth UI, Providers, and auth nav button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 import { Nunito_Sans, Chivo } from "next/font/google";
 import "./globals.css";
+import Providers from "../components/auth/Providers";
+  
 
 const nunito = Nunito_Sans({
   subsets: ["latin"],
@@ -27,12 +29,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${nunito.variable} ${chivo.variable}`}>
-      <body>
-  <Header />
-  {children}
-  <Footer />
-</body>
+    <html
+      lang="en"
+      className={`${nunito.variable} ${chivo.variable}`}
+      suppressHydrationWarning
+    >
+      <body suppressHydrationWarning>
+        <Providers>
+          <Header />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
     </html>
   );
 }
